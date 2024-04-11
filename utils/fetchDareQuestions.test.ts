5c30c4f594be9808e91600e79e869916bdfb56d1
Iamvetle <vetle122@gmail.com>
2024-04-11 11:48:32 +0200

    added some tests for fetchDareQuestions - but not testing the response  yet

--- utils/fetchDareQuestions.test.ts
+++ utils/fetchDareQuestions.test.ts
@@ -6,12 +6,14 @@ describe('fetchDareQuestions', () => {
     test('Should be (the function) defined', () => {
         expect(fetchDareQuestions).toBeDefined()
     })
+    test('Should not throw an array', async () => {
+        expect(fetchDareQuestions).not.toThrow()
+    })
     test('Should return an array', async () => {
         const response = await fetchDareQuestions()
         expectTypeOf(response).toBeArray()
-        assert.isArray(response)
-        expect(fetchDareQuestions).not.toThrow()
     })
+    
     // test('Should have a length/some items (over three)', async () => {
     //     const response = await fetchDareQuestions()
     //     expect(response.length).toBeGreaterThan(3)        
