0458a75000f8bfb7e204c92364b39231c0db6f95
Iamvetle <vetle122@gmail.com>
2024-05-05 00:10:17 +0200

    Trying to make to work as pwa

--- app.vue
+++ app.vue
@@ -1,204 +1,245 @@
 <template>
-  <Head>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <link rel="manifest" href="/site.webmanifest" />
-  </Head>
+	<div id="site-wrapper">
+		<div
+			class="bg-[#F0EDCC] w-full min-h-screen pt-12 px-2 flex flex-col mx-auto"
+		>
+			<!-- <SpeedInsights /> -->
 
-  <div
-    class="bg-[#F0EDCC] w-full min-h-screen pt-12 px-2 flex flex-col mx-auto"
-  >
-    <!-- <SpeedInsights /> -->
+			<div class="space-y-2 w-fit mx-auto">
+				<h1 class="text-3xl text-[#02343F] text-center font-bold">
+					Truth or dare?
+				</h1>
+			</div>
 
-    <div class="space-y-2 w-fit mx-auto">
-      <h1 class="text-3xl text-[#02343F] text-center font-bold">
-        Truth or dare?
-      </h1>
-      <div></div>
-    </div>
+			<div
+				class="max-w-96 mx-auto mt-10 flex flex-col w-full justify-center"
+			>
+				<div class="flex justify-between">
+					<!-- TODO - Make it so I can ask the questions in norwegian too -->
+					<!-- <Language :language="languageText" class="flex mb-2" @switch-language="switchLanguage" /> -->
+					<!-- <span>{{ mode }}</span> -->
+				</div>
 
-    <div class="max-w-96 mx-auto mt-10 flex flex-col w-full justify-center">
-      <div class="flex justify-between">
-        <!-- TODO - Make it so I can ask the questions in norwegian too -->
-        <!-- <Language :language="languageText" class="flex mb-2" @switch-language="switchLanguage" /> -->
-        <!-- <span>{{ mode }}</span> -->
-      </div>
+				<!-- 
+          * This is causing layout shifts - I should not have an animation on initial load
+          <TheQuestion
+					:text="questionText"
+					class="bg-white rounded-lg flex items-center justify-start min-h-64 w-full px-4"
+					v-motion="slideAnimation"
+					:delay="200"
+					:key="questionText"
+				/> -->
 
-      <TheQuestion
-        :text="questionText"
-        class="bg-white rounded-lg flex items-center justify-start min-h-64 w-full px-4"
-        v-motion="slideAnimation"
-        :delay="200"
-        :key="questionText"
-      />
-      <!-- <p v-if="theQuestion" class="break-words">{{ questionText }}</p> -->
+				<TheQuestion
+					:text="questionText"
+					class="bg-white rounded-lg flex items-center justify-start min-h-64 w-full px-4"
+					:key="questionText"
+				/>
+				<!-- <p v-if="theQuestion" class="break-words">{{ questionText }}</p> -->
 
-      <div class="text-[#02343F] flex justify-between space-x-2 mt-7 text-sm">
-        <BackButton @back="backToQuestion" />
-        <ForwardButton @forward="forwardToQuestion" />
-      </div>
+				<div
+					class="text-[#02343F] flex justify-between space-x-2 mt-7 text-sm"
+				>
+					<BackButton @back="backToQuestion" />
+					<ForwardButton @forward="forwardToQuestion" />
+				</div>
 
-      <div class="mb-56 mt-14 flex justify-between space-x-2">
-        <TruthButton
-          :label="language === 'en' ? 'Truth' : 'Sannhet'"
-          class="font-medium text-[#50586C] bg-[#DCE2F0] max-w-40 w-full h-16 rounded-md shadow-sm"
-          @truth="truthButtonClick"
-        />
-        <DareButton
-          :label="language === 'en' ? 'Dare' : 'Nødt'"
-          class="font-medium text-[#9000FF] bg-[#FFE8F5] max-w-40 w-full h-16 rounded-md shadow-sm"
-          @dare="dareButtonClick"
-        />
-      </div>
-    </div>
-    <!-- <pre>{{ theQuestion }}</pre> -->
-  </div>
+				<div class="mb-56 mt-14 flex justify-between space-x-2">
+					<TruthButton
+						:label="language === 'en' ? 'Truth' : 'Sannhet'"
+						class="font-medium text-[#50586C] bg-[#DCE2F0] max-w-40 w-full h-16 rounded-md shadow-sm"
+						@truth="truthButtonClick"
+					/>
+					<DareButton
+						:label="language === 'en' ? 'Dare' : 'Nødt'"
+						class="font-medium text-[#9000FF] bg-[#FFE8F5] max-w-40 w-full h-16 rounded-md shadow-sm"
+						@dare="dareButtonClick"
+					/>
+				</div>
+			</div>
+			<!-- <pre>{{ theQuestion }}</pre> -->
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
-import { slideVisibleLeft, slideVisibleRight } from "@vueuse/motion";
-import { fetchDareQuestions } from "./utils/fetchDareQuestions";
-import { fetchTruthQuestions } from "./utils/fetchTruthQuestions";
-// import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+	import { slideVisibleLeft, slideVisibleRight } from "@vueuse/motion";
+	import { fetchDareQuestions } from "./utils/fetchDareQuestions";
+	import { fetchTruthQuestions } from "./utils/fetchTruthQuestions";
+	// import { SpeedInsights } from "@vercel/speed-insights/nuxt";
 
-type Question = {
-  en: string;
-  no: string;
-};
+	type Question = {
+		en: string;
+		no: string;
+	};
 
-const allDareQuestions = ref<Question[] | []>([]);
-const numOfDareQuestions = computed(() => allDareQuestions.value.length);
+	useSeoMeta({
+		title: "Truth or dare",
+		description: "Truth or dare game that you can play with friends",
+		viewport: "width=device-width, initial-scale=1",
+	});
 
-const allTruthQuestions = ref<Question[] | []>([]);
-const numOfTruthQuestions = computed(() => allTruthQuestions.value.length);
+	useHead({
+		htmlAttrs: {
+			lang: "en",
+		},
 
-const theQuestion = ref<Question | null>(null);
-const backQuestions = ref<Question[] | []>([]);
-const forwardQuestions = ref<Question[] | []>([]);
-const mode = ref<"truth" | "dare">("truth");
+		meta: [
+			{
+				name: "theme-color",
+				content: "#ffffff",
+			},
+		],
+		link: [
+			{
+				rel: "icon",
+				href: "/favicon.ico",
+			},
+			{
+				rel: "manifest",
+				href: "/manifest.json",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				href: "/apple-touch-icon.png",
+			},
+		],
+	});
 
-const slideDirection = ref<"right" | "left">("right");
+	const allDareQuestions = ref<Question[] | []>([]);
+	const numOfDareQuestions = computed(() => allDareQuestions.value.length);
 
-const slideAnimation = computed(() =>
-  slideDirection.value === "right" ? slideVisibleRight : slideVisibleLeft
-);
+	const allTruthQuestions = ref<Question[] | []>([]);
+	const numOfTruthQuestions = computed(() => allTruthQuestions.value.length);
 
-onMounted(async () => {
-  allDareQuestions.value = await fetchDareQuestions();
-  allTruthQuestions.value = await fetchTruthQuestions();
+	const theQuestion = ref<Question | null>(null);
+	const backQuestions = ref<Question[] | []>([]);
+	const forwardQuestions = ref<Question[] | []>([]);
+	const mode = ref<"truth" | "dare">("truth");
 
-  console.log("app 91", allTruthQuestions.value)
+	const slideDirection = ref<"right" | "left">("right");
 
-  if (allTruthQuestions.value.length > 1) {
-    console.log("api 94", allTruthQuestions.value)
-    console.log("api 98", theQuestion.value);
+	const slideAnimation = computed(() =>
+		slideDirection.value === "right" ? slideVisibleRight : slideVisibleLeft
+	);
 
+	onMounted(async () => {
+		allDareQuestions.value = await fetchDareQuestions();
+		allTruthQuestions.value = await fetchTruthQuestions();
 
-    theQuestion.value = randomTruthQuestion();
-    console.log("api 97", theQuestion.value);
-    
-  }
+		console.log("app 91", allTruthQuestions.value);
 
-});
+		if (allTruthQuestions.value.length > 1) {
+			console.log("api 94", allTruthQuestions.value);
+			console.log("api 98", theQuestion.value);
 
-const truthButtonClick = () => {
-  const question = randomTruthQuestion();
+			theQuestion.value = randomTruthQuestion();
+			console.log("api 97", theQuestion.value);
+		}
+	});
 
-  console.log(question)
+	const truthButtonClick = () => {
+		const question = randomTruthQuestion();
 
-  if (theQuestion.value != null) {
-    slideDirection.value = "left";
+		console.log(question);
 
-    //@ts-ignore
-    backQuestions.value.push(theQuestion.value);
-  }
-  theQuestion.value = question;
+		if (theQuestion.value != null) {
+			slideDirection.value = "left";
 
-  mode.value = "truth";
-};
+			//@ts-ignore
+			backQuestions.value.push(theQuestion.value);
+		}
+		theQuestion.value = question;
 
-const dareButtonClick = () => {
-  const question = randomDareQuestion();
+		mode.value = "truth";
+	};
 
-  if (theQuestion.value != null) {
-    slideDirection.value = "right";
+	const dareButtonClick = () => {
+		const question = randomDareQuestion();
 
-    //@ts-ignore
-    backQuestions.value.push(theQuestion.value);
-  }
-  theQuestion.value = question;
-  mode.value = "dare";
-};
+		if (theQuestion.value != null) {
+			slideDirection.value = "right";
 
-// TODO - Make it so I can ask the questions in norwegian too
-// const languageText = computed(() =>
-//   language.value === "en" ? "english" : "norsk"
-// );
-const language = ref<"en" | "no">("en");
-// const switchLanguage = () => {
-//   if (language.value === "en") {
-//     return (language.value = "no");
-//   }
+			//@ts-ignore
+			backQuestions.value.push(theQuestion.value);
+		}
+		theQuestion.value = question;
+		mode.value = "dare";
+	};
 
-//   if (language.value === "no") {
-//     return (language.value = "en");
-//   }
-// };
+	// TODO - Make it so I can ask the questions in norwegian too
+	// const languageText = computed(() =>
+	//   language.value === "en" ? "english" : "norsk"
+	// );
+	const language = ref<"en" | "no">("en");
+	// const switchLanguage = () => {
+	//   if (language.value === "en") {
+	//     return (language.value = "no");
+	//   }
 
-const questionText = computed(() => {
-  if (language.value === "en") {
-    return theQuestion.value?.en;
-  }
-  if (language.value === "no") {
-    return theQuestion.value?.no;
-  }
-});
+	//   if (language.value === "no") {
+	//     return (language.value = "en");
+	//   }
+	// };
 
-const backToQuestion = () => {
-  if (backQuestions.value.length > 0) {
-    // Updates the "forward" questions
-    //@ts-ignore
-    forwardQuestions.value.push(theQuestion.value);
+	const questionText = computed(() => {
+		if (language.value === "en") {
+			return theQuestion.value?.en;
+		}
+		if (language.value === "no") {
+			return theQuestion.value?.no;
+		}
+	});
 
-    // Removes an earlier question and assigns it to current question
-    theQuestion.value = backQuestions.value.pop() as Question;
-  }
-};
+	const backToQuestion = () => {
+		if (backQuestions.value.length > 0) {
+			// Updates the "forward" questions
+			//@ts-ignore
+			forwardQuestions.value.push(theQuestion.value);
 
-function forwardToQuestion () {
-  if (forwardQuestions.value.length > 0) {
-    const forwardQuestion = forwardQuestions.value.pop() as Question;
-    // updates the earlier questions
-    //@ts-ignore
-    backQuestions.value.push(theQuestion.value);
+			// Removes an earlier question and assigns it to current question
+			theQuestion.value = backQuestions.value.pop() as Question;
+		}
+	};
 
-    // updates the current question
-    theQuestion.value = forwardQuestion;
-  }
-};
+	function forwardToQuestion() {
+		if (forwardQuestions.value.length > 0) {
+			const forwardQuestion = forwardQuestions.value.pop() as Question;
+			// updates the earlier questions
+			//@ts-ignore
+			backQuestions.value.push(theQuestion.value);
 
-function randomTruthQuestion () {
-  // const response = await $fetch<Question[]>("/truthQuestions.json")
+			// updates the current question
+			theQuestion.value = forwardQuestion;
+		}
+	}
 
-  const randomNumber = Math.floor(Math.random() * numOfTruthQuestions.value);
+	function randomTruthQuestion() {
+		// const response = await $fetch<Question[]>("/truthQuestions.json")
 
-  const randomQuestion = allTruthQuestions.value[randomNumber];
-  allTruthQuestions.value.splice(randomNumber, 1);
+		const randomNumber = Math.floor(
+			Math.random() * numOfTruthQuestions.value
+		);
 
-  // console.assert(truthQuestions.value.length > 0, "No new truth questions remaining")
+		const randomQuestion = allTruthQuestions.value[randomNumber];
+		allTruthQuestions.value.splice(randomNumber, 1);
 
-  return randomQuestion;
-};
+		// console.assert(truthQuestions.value.length > 0, "No new truth questions remaining")
 
-const randomDareQuestion = () => {
-  const randomNumber = Math.floor(Math.random() * numOfDareQuestions.value);
+		return randomQuestion;
+	}
 
-  const randomQuestion = allDareQuestions.value[randomNumber];
-  allDareQuestions.value.splice(randomNumber, 1);
+	const randomDareQuestion = () => {
+		const randomNumber = Math.floor(
+			Math.random() * numOfDareQuestions.value
+		);
 
-  // console.assert(dareQuestions.value.length > 0, "No new dare questions remaining" )
+		const randomQuestion = allDareQuestions.value[randomNumber];
+		allDareQuestions.value.splice(randomNumber, 1);
 
-  return randomQuestion;
-};
+		// console.assert(dareQuestions.value.length > 0, "No new dare questions remaining" )
+
+		return randomQuestion;
+	};
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-  '@nuxtjs/tailwindcss',
-  '@nuxt/test-utils/module',
-  '@vueuse/motion/nuxt'
-  ]
-})
- 
+	devtools: { enabled: true },
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@nuxt/test-utils/module",
+		"@vueuse/motion/nuxt",
+	],
+	nitro: {
+		prerender: {
+			// Have to specify this (npx nuxi generate) or manifest.json will become a html file..
+			ignore: ["/manifest.json"],
+		},
+	},
+});
